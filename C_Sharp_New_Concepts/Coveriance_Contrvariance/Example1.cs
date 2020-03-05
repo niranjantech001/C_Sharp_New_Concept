d13f483bbaadb6f46e50d0ea881f950a8f5ad99b
niranjantech001@gmail.com <Vijayank@123>
2020-03-05 08:31:40 +0530

    vv

---
+++ C_Sharp_New_Concepts/Coveriance_Contrvariance/Example1.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace C_Sharp_New_Concepts.Coveriance_Contrvariance
+//{
+//    class Example1
+//    {
+
+      
+//        static void Main()
+//        {
+//           // Assigment Compitabality
+//            string str = "test";
+//            object obj = str;
+
+//            // Coveriance
+
+//            IEnumerable<string> strings = new List<string>();
+
+//            // An object that is instantiated with a more derived type argument   
+//            // is assigned to an object instantiated with a less derived type argument.   
+//            // Assignment compatibility is preserved.  
+
+//            IEnumerable<object> objects = strings;
+
+//            // Contravariance. 
+
+           
+
+//        }
+//    }
+//}
